b4d71d051e88f2603d398a6beec6861e2e162eef
Josue Hoenicka <josuee.vzla@gmail.com>
2022-12-23 08:25:00 -0300

    update: styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 function MCD(){
-    if (arguments.length<2) return false;
-    if (arguments.length==2)return (arguments[1]==0?arguments[0]:MCD(arguments[1],arguments[0]%arguments[1]));
+    if (arguments.length < 2) {
+        return false;
+    }
+    if (arguments.length == 2) {
+        return (arguments[1] == 0 ? arguments[0] : 
+            MCD(arguments[1],arguments[0]%arguments[1]));
+    }
     var arr=[].splice.call(arguments,0);
     arr.splice(0,2,MCD(arr[0],arr[1]));
     return MCD.apply(window,arr);
 }
 
-// Funcion que calcula el mcm
 function mcm(a,b){
 if (arguments.length<2) return false;
 if (arguments.length==2)return arguments[0]*arguments[1]/MCD(arguments[0],arguments[1]);
